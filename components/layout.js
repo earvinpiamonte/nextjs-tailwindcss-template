5192eeb6d2c9b5b90ca0559a4a88922ac46b8453
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-11-28 00:25:43 +0800

    Remove duplicate `og:image`

--- components/layout.js
+++ components/layout.js
@@ -15,12 +15,6 @@ export default function Layout({ children, home }) {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content={siteMetaData.description} />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteMetaData.title
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
 
         {/* Twitter */}
         <meta name="twitter:card" content="summary" key="twcard" />
