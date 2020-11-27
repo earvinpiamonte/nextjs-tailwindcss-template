0c7bdb20de3d658e68ba63a5619358090f7781b0
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-11-28 01:36:09 +0800

    Upgrade Tailwind CSS via PostCSS 7 compatibility build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
-  // ...
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
 };
