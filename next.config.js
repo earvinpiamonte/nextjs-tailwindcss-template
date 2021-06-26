9ee4d529375115aa17e6937b9893ec2e84deb4f5
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2021-06-26 12:34:42 +0800

    Update `site.url` config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ module.exports = {
     site: {
       name: "Next.js + Tailwind CSS template",
       url:
-        process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/",
+        process.env.NODE_ENV === "development"
+          ? "http://localhost:3000"
+          : "https://earvinpiamonte-nextjs-tailwindcss-template.vercel.app",
       title: "Next.js + Tailwind CSS template",
       description: "Next.js + Tailwind CSS template",
       socialPreview: "/images/preview.png",
