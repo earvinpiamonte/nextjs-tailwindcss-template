0d3189c5dfc03531317177a69f0c1c5c1e49bea9
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2022-04-14 20:18:30 +0800

    Update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import getConfig from "next/config";
+import getConfig from 'next/config';
 
-import Layout from "@/components/Layout";
+import Layout from '@/components/Layout';
 
 const { publicRuntimeConfig } = getConfig();
 const { name } = publicRuntimeConfig.site;
@@ -16,18 +16,15 @@ const Home = () => {
           <h2>Additions:</h2>
           <ul>
             <li>
-              Pre-configured <code>postcss.config.js</code>.
-            </li>
-            <li>
-              Pre-configured <code>tailwind.config.js</code>.
+              Pre-configured <code>postcss.config.js</code> and{' '}
+              <code>tailwind.config.js</code>.
             </li>
             <li>
               <code>global.css</code> that contains Tailwind CSS directives.
             </li>
             <li>
-              <code>{"<Layout />"}</code> component.
+              <code>{'<Layout />'}</code> component.
             </li>
-            <li>Purging styles using PurgeCSS.</li>
           </ul>
 
           <h2>Guides</h2>
@@ -56,7 +53,7 @@ const Home = () => {
 
           <h2>Maintainer</h2>
           <p>
-            This project template is maintained by{" "}
+            This project template is maintained by{' '}
             <a
               href="https://earvinpiamonte.com"
               target="_blank"
@@ -64,8 +61,8 @@ const Home = () => {
               className="app-link"
             >
               @earvinpiamonte
-            </a>{" "}
-            and publicly available on{" "}
+            </a>{' '}
+            and publicly available on{' '}
             <a
               href="https://github.com/earvinpiamonte/nextjs-tailwindcss-template"
               target="_blank"
