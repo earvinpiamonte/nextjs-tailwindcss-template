ca1da9f2942e17032310505e9cea6d2d068422e4
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2022-04-14 22:53:08 +0800

    Add `@tailwindcss/forms`

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,5 @@ module.exports = {
     extend: {},
   },
   variants: {},
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 };
